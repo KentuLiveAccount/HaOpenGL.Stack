8ea28baf4e4518b7a98c411925622f998f1ab389
Kentaro Urata <kentu@microsoft.com>
2022-12-18 14:02:32 -0800

    improving via tutorial
    added call to preservingMatrix
    added call to color3f
    added call to postRedisplay
    added call to swapBuffers, in combination with DoubleBuffered

--- HaOpenGL/app/Main.hs
+++ HaOpenGL/app/Main.hs
@@ -7,13 +7,13 @@ import Data.IORef
 
 divisor = 24
 
-data AppState = AS {step :: GLfloat}
+data AppState = AS {step :: GLfloat, interval :: Timeout}
 
 initialAppState :: AppState
-initialAppState = (AS 0.0)
+initialAppState = (AS 0.0 16)
 
 nextAppState :: AppState -> AppState
-nextAppState (AS st) = (AS st')
+nextAppState (AS st to) = (AS st' to)
   where
     st' = if st + increment > 1.0 then 0.0 else st + increment
     increment = 1.0 / divisor
@@ -22,14 +22,19 @@ toRect :: (GLfloat,GLfloat,GLfloat) -> [(GLfloat,GLfloat,GLfloat)]
 toRect (x, y, z) = [(x, y, z), (x + 0.1, y, z), (x + 0.1, y + 0.1, z), (x, y + 0.1, z)]
 
 myPoints :: GLfloat -> [(GLfloat,GLfloat,GLfloat)]
-myPoints dl = concatMap toRect $ map (\(x, y, z) -> (x * 0.9, y * 0.9, z * 0.9)) [ (sin (delta + 2*pi*k/divisor), cos (delta + 2*pi*k/divisor), 0) | k <- [1..divisor] ]
+myPoints dl = concatMap toRect $ map (\(x, y, z) -> (x * 0.9, y * 0.9, z * 0.9)) 
+  [ (sin (delta + 2*pi*k/divisor), cos (delta + 2*pi*k/divisor), 0) | k <- [1..divisor] ]
     where
         delta = 2 * pi / divisor * dl
+
+color3f :: GLfloat -> GLfloat -> GLfloat -> IO ()
+color3f r g b = color $ Color3 r g b
 
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
   stateRef <- newIORef initialAppState
+  initialDisplayMode $= [DoubleBuffered]
   _window <- createWindow "Hello World"
   displayCallback $= (display stateRef)
   addTimerCallback 16 (timerProc stateRef)
@@ -37,14 +42,19 @@ main = do
 
 display :: IORef AppState -> DisplayCallback
 display ior = do 
-  (AS dl) <- readIORef ior
-  clear [ColorBuffer]
-  renderPrimitive Quads $
-     mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) $ myPoints dl
-  flush
+  (AS dl _) <- readIORef ior
+  clear [ColorBuffer] -- ColorBuffer :: ClearBuffer
+  preservingMatrix ( do
+    scale 0.8 0.8 (0.8::GLfloat)
+    -- renderPrimitive :: PrimitiveMode -> IO a -> IO a
+    renderPrimitive Quads $ do
+      color3f 1 0 0
+      mapM_ (\(x, y, z) ->  (vertex $ Vertex3 x y z)) $ myPoints dl)
+  swapBuffers
 
 timerProc :: IORef AppState -> IO ()
 timerProc ior = do
-    addTimerCallback 16 $ timerProc ior
+    (AS _ timeout) <- readIORef ior
+    addTimerCallback timeout $ timerProc ior
     modifyIORef ior nextAppState
-    display ior
+    postRedisplay Nothing
